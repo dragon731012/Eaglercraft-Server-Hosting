a7ad2deaed40dfc68f3a1547eba8c4960c0c7e89
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-04-10 17:50:40 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
-cd server
-java -jar server.jar &
-cd ..
 cd bungee
 java -jar bungee.jar &
+cd ..
+cd server
+java -jar server.jar
 cd ..
 sed -i 's/online-mode=true/online-mode=false/' server/server.properties
